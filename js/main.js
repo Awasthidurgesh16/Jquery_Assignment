a8502c96ec9b9aaf2d1d955779220faff165b4e9
Durgesh Awasthi <awasthi.durgesh16@gmail.com>
2016-11-25 16:40:06 +0530

    Add files via upload
    Pagination left

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-
+      $(".content").hide();
     $("#do-search").on('click',function(){
+        
         
 
         var $answerContainer=$('#answer');
@@ -13,6 +14,7 @@ $(document).ready(function(){
 
         //Grab our container and assign it to variable for later use
         //var container = $('#container');
+        $(".content").show();
 
         $.ajax({
             method: 'GET',
@@ -25,20 +27,37 @@ $(document).ready(function(){
                     console.log(res);
                         
                          //$answerContainer.append('<tr><th><img src="' +res.Poster+''> + '</th><th>' + res.Title + '</th><th>' +res.Type + '</th><th>' + res.Year +'</th></tr>');                         
-                    $answerContainer.append('<li><img src="'+res.Poster+'"/></li>'
-                        +'<li>'+res.Title+'</li>'
-                       +'<li>'+res.Type+'</li>'
-                        +'<li>'+res.Year+'</li>')  ;                     
+                    // $answerContainer.append('<li><img src="'+res.Poster+'"/></li>'
+                    //     +'<li>'+res.Title+'</li>'
+                    //    +'<li>'+res.Type+'</li>'
+                    //     +'<li>'+res.Year+'</li>'
+                    //     +'<li>'+res.imdbID+'</li>')  ;  
+                                if(res.Poster=="N/A"){
+                                        res.Poster="img/not-found.png";
+                                }
+                    
+                            $answerContainer.append('<div class="row ">'+
+                            '<div class="col-md-2 col-md-offset-1"><img src="'+ res.Poster +'" ></div>'+
+                            '<div class="col-md-2">'+ res.Title +'</div>'+
+                            '<div class="col-md-2">'+ res.Year +'</div>'+
+                            '<div class="col-md-2">'+ res.Type +'</div>'+
+                            '<div class="col-md-2">'+ res.imdbID +'</div>'+'</div>');
+
+
                     
                 });
 
 
-                },
+                    },
             error: function(error){
                 console.error('@ERROR', error);
             }
     });
-        
+$(selector).pagination({
+        items: 100,
+        itemsOnPage: 10,
+        cssStyle: 'light-theme'
+    });        
 
 });
 
